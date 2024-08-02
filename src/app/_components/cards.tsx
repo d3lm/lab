fe285688c8ecb2d6b7ddd24dd719cd46ed33e2e2
vaunblu <vaunblu@proton.me>
2024-08-02 16:24:18 -0400

    chore: refine touch controls

--- src/app/_components/cards.tsx
+++ src/app/_components/cards.tsx
@@ -7,6 +7,7 @@ import useMeasure from "react-use-measure";
 import { useTransform } from "framer-motion";
 
 export function Cards() {
+  const [activeCard, setActiveCard] = React.useState(0);
   const [ref, screen] = useMeasure();
   const mousePosition = useMousePosition();
 
@@ -37,30 +38,68 @@ export function Cards() {
     >
       <div className="absolute bottom-[180px] left-0 w-full">
         <div className="relative">
-          <div className="absolute w-full px-6">
+          <motion.div
+            layout
+            animate={activeCard === 1 ? { top: "-40vh" } : {}}
+            className="absolute w-full px-6"
+          >
             <motion.div
-              whileTap={{ rotate: rotate.get(), translateY: translateY.get() }}
+              whileTap={{
+                rotate: rotate.get() * (activeCard === 1 ? -1 : 1),
+                translateY: translateY.get() * (activeCard === 1 ? -1 : 1),
+              }}
+              onClick={() =>
+                setActiveCard((prevState) => {
+                  if (prevState === 1) return 0;
+                  return 1;
+                })
+              }
               className="aspect-[1.586] rounded-2xl bg-foreground/70 p-6 text-background"
             >
               Card 1
             </motion.div>
-          </div>
-          <div className="absolute top-[60px] w-full px-6">
+          </motion.div>
+          <motion.div
+            layout
+            animate={activeCard === 2 ? { top: "calc(-40vh - 60px)" } : {}}
+            className="absolute top-[60px] w-full px-6"
+          >
             <motion.div
-              whileTap={{ rotate: rotate.get(), translateY: translateY.get() }}
+              whileTap={{
+                rotate: rotate.get() * (activeCard === 2 ? -1 : 1),
+                translateY: translateY.get() * (activeCard === 2 ? -1 : 1),
+              }}
+              onClick={() =>
+                setActiveCard((prevState) => {
+                  if (prevState === 2) return 0;
+                  return 2;
+                })
+              }
               className="bg-foreground/85 aspect-[1.586] rounded-2xl p-6 text-background"
             >
               Card 2
             </motion.div>
-          </div>
-          <div className="absolute top-[120px] w-full px-6">
+          </motion.div>
+          <motion.div
+            animate={activeCard === 3 ? { top: "calc(-30vh - 120px)" } : {}}
+            className="absolute top-[120px] w-full px-6"
+          >
             <motion.div
-              whileTap={{ rotate: rotate.get(), translateY: translateY.get() }}
+              whileTap={{
+                rotate: rotate.get() * (activeCard === 3 ? -1 : 1),
+                translateY: translateY.get() * (activeCard === 3 ? -1 : 1),
+              }}
+              onClick={() =>
+                setActiveCard((prevState) => {
+                  if (prevState === 3) return 0;
+                  return 3;
+                })
+              }
               className="aspect-[1.586] rounded-2xl bg-foreground p-6 text-background"
             >
               Card 3
             </motion.div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
