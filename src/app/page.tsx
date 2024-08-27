c25a0b7d6ffacde53271aef5f50565240a0aed6e
vaunblu <vaunblu@proton.me>
2024-08-27 07:33:50 -0400

    chore: wireframe of vinyl player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,20 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { AnimatePresence, motion, MotionConfig } from "framer-motion";
+import { MotionConfig } from "framer-motion";
 import React from "react";
-import Image from "next/image";
-import imagePerson from "@/assets/person.jpg";
-import { ChevronRight } from "lucide-react";
 
 export default function HomePage() {
-  const [open, setOpen] = React.useState(true);
-
   return (
     <MotionConfig transition={{ type: "spring", bounce: 0, duration: 0.8 }}>
-      <div
-        onClick={() => setOpen((prev) => !prev)}
-        className="relative flex h-screen items-center justify-center"
-      >
-        <motion.div
-          initial={{ backdropFilter: "blur(0px)" }}
-          animate={
-            open
-              ? {
-                  backdropFilter: "blur(14px)",
-                  backgroundColor: "rgb(0 0 0 / 0.4)",
-                }
-              : {}
-          }
-          className="size-full absolute left-0 top-0"
-        />
+      <div className="relative flex h-screen items-center justify-center">
+        <div className="size-[520px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-200" />
+        <div className="size-[400px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-300" />
+        <div className="size-[260px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-400" />
+        <div className="size-[200px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-500" />
+        <div className="size-[90px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-600" />
+        <div className="size-[60px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-700" />
+        <div className="size-[25px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-800" />
+        <div className="size-[15px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900" />
       </div>
     </MotionConfig>
   );
