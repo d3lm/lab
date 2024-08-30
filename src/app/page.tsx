a46c57076d30e9a655c57f31f05a5e64dc406182
vaunblu <vaunblu@proton.me>
2024-08-30 17:30:29 -0400

    chore: add notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import imageThunderBg from "@/assets/thunder-bg.png";
 import imageAzura from "@/assets/azura.png";
 import Image from "next/image";
 
+// NOTE: TOOK 30 MINS
+
 const transition: Transition = {
   type: "spring",
   damping: 80,
--- src/app/thunder-speed/page.tsx
+++ src/app/thunder-speed/page.tsx
@@ -6,6 +6,8 @@ import imageThunderBg from "@/assets/thunder-bg.png";
 import imageAzura from "@/assets/azura.png";
 import Image from "next/image";
 
+// NOTE: TOOK 1 HOUR
+
 const transition: Transition = {
   type: "spring",
   damping: 80,
