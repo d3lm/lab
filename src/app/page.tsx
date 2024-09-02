4d83ac5da603cb1ba141bfab2bf5ad58b21fe7f0
vaunblu <vaunblu@proton.me>
2024-09-02 09:27:01 -0400

    fons 22

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,8 +38,7 @@ export default function HomePage() {
               <div className="relative h-full w-[30%] bg-pink-200 bg-[linear-gradient(60deg,rgba(32,34,98,1)_0%,rgba(59,88,135,1)_15%,rgba(172,172,236,1)_60%,rgba(203,237,253,1)_90%,rgba(222,243,254,1)_100%)]">
                 <div className="size-full absolute left-0 top-0 bg-gradient-to-l from-[rgba(222,243,254,0.5)] to-40%" />
               </div>
-              <div className="relative h-full w-[20%] bg-pink-400" />
-              {/* <div className="relative h-full w-[30%] bg-pink-400 bg-[linear-gradient(310deg,rgba(175,196,239,1)_0%,rgba(203,237,253,1)_59%,rgba(222,243,254,1)_100%)]" /> */}
+              <div className="relative h-full w-[20%] bg-pink-400 bg-[linear-gradient(310deg,rgba(175,196,239,1)_0%,rgba(203,237,253,1)_59%,rgba(222,243,254,1)_100%)]" />
               {/* <div className="size-full absolute left-0 top-0 backdrop-blur-sm" /> */}
               {/* <div className="size-full absolute left-0 top-0 opacity-20 [filter:url(#grain)]" /> */}
             </div>
