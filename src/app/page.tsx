9fd41c3d4a14c3de0b9b2e7925c01d05b6f3e2a2
vaunblu <vaunblu@proton.me>
2024-09-05 08:47:31 -0400

    chore: initial setup of the image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { motion, MotionConfig, type Transition } from "framer-motion";
+import Image from "next/image";
 import React from "react";
+import imageBarn from "@/assets/barn.jpg";
 
 const transition: Transition = { type: "spring", bounce: 0, duration: 0.4 };
 
@@ -14,8 +16,31 @@ function InnerContent() {
   const ctx = React.useContext(Context);
 
   return (
-    <div>
-      <p>Start</p>
+    <div className="size-[600px] relative overflow-hidden rounded-[100px] border border-[#cacaca] bg-[#e3e3e3] font-medium tracking-tight shadow-[0_3px_3px_-1.5px_rgba(0,0,0,0.06)]">
+      <Image
+        src={imageBarn}
+        alt="Barn"
+        className="size-full scale-150 object-cover"
+      />
+
+      <div className="absolute left-0 top-0 h-1/2 w-full backdrop-blur-md [mask:linear-gradient(180deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_100%)]" />
+      <div className="absolute left-0 top-0 h-1/2 w-full bg-gradient-to-b from-black/40" />
+
+      <div className="absolute left-0 top-0 p-14 px-16">
+        <p className="text-3xl font-normal leading-[1.4] tracking-wide text-white">
+          Snowy Barn
+          <br />
+          Paradise
+          <br />
+          WA
+        </p>
+      </div>
+      <div className="absolute right-0 top-0 flex gap-1 p-[75px] px-16">
+        <div className="h-[3px] w-3.5 rounded-full bg-white/30" />
+        <div className="h-[3px] w-3.5 rounded-full bg-white/30" />
+        <div className="h-[3px] w-3.5 rounded-full bg-white" />
+        <div className="h-[3px] w-3.5 rounded-full bg-white/30" />
+      </div>
     </div>
   );
 }
