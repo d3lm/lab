1ea6f9e4e49070f0b606f5af7edf9da4c45d2859
vaunblu <vaunblu@proton.me>
2024-09-05 09:54:01 -0400

    chore: finished direction aware carosel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,227 @@
 "use client";
 
-import { motion, MotionConfig, type Transition } from "framer-motion";
+import {
+  AnimatePresence,
+  motion,
+  MotionConfig,
+  type Transition,
+} from "framer-motion";
 import Image from "next/image";
 import React from "react";
 import imageBarn from "@/assets/barn.jpg";
+import imageOrlando from "@/assets/orlando.jpg";
+import imageSnow from "@/assets/snow-board.jpg";
+import imageField from "@/assets/field.jpg";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const Context = React.createContext<{
+  index: number;
+  setIndex: React.Dispatch<React.SetStateAction<number>>;
+  status: string;
+  setStatus: React.Dispatch<React.SetStateAction<string>>;
+}>({ index: 0, setIndex: () => null, status: "", setStatus: () => null });
 
 const transition: Transition = { type: "spring", bounce: 0, duration: 0.4 };
 
-const Context = React.createContext<{
-  status: string;
-  setStatus: React.Dispatch<React.SetStateAction<string>>;
-}>({ status: "", setStatus: () => null });
+const locationData = [
+  {
+    title: "Orlando Beach",
+    city: "Orlando",
+    state: "FL",
+    image: imageOrlando,
+  },
+  {
+    title: "Mount Elbert",
+    city: "Leadville",
+    state: "CO",
+    image: imageSnow,
+  },
+  {
+    title: "Mount Rainier",
+    city: "Paradise",
+    state: "WA",
+    image: imageBarn,
+  },
+  {
+    title: "Galt Ranch",
+    city: "White Sulphur Springs",
+    state: "MT",
+    image: imageField,
+  },
+];
 
-function InnerContent() {
+function NavigationIndicator() {
   const ctx = React.useContext(Context);
 
   return (
-    <div className="size-[600px] relative overflow-hidden rounded-[100px] border border-[#cacaca] bg-[#e3e3e3] font-medium tracking-tight shadow-[0_3px_3px_-1.5px_rgba(0,0,0,0.06)]">
-      <Image
-        src={imageBarn}
-        alt="Barn"
-        className="size-full scale-150 object-cover"
+    <div className="absolute right-0 top-0 z-10 flex gap-1 p-[75px] px-16">
+      <div
+        className={cn(
+          "h-[3px] w-3.5 rounded-full bg-white/30 transition-colors",
+          {
+            "bg-white": ctx.index === 0,
+          },
+        )}
       />
+      <div
+        className={cn(
+          "h-[3px] w-3.5 rounded-full bg-white/30 transition-colors",
+          {
+            "bg-white": ctx.index === 1,
+          },
+        )}
+      />
+      <div
+        className={cn(
+          "h-[3px] w-3.5 rounded-full bg-white/30 transition-colors",
+          {
+            "bg-white": ctx.index === 2,
+          },
+        )}
+      />
+      <div
+        className={cn(
+          "h-[3px] w-3.5 rounded-full bg-white/30 transition-colors",
+          {
+            "bg-white": ctx.index === 3,
+          },
+        )}
+      />
+    </div>
+  );
+}
 
-      <div className="absolute left-0 top-0 h-1/2 w-full backdrop-blur-md [mask:linear-gradient(180deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_100%)]" />
-      <div className="absolute left-0 top-0 h-1/2 w-full bg-gradient-to-b from-black/40" />
+function PreviousButton() {
+  const ctx = React.useContext(Context);
 
-      <div className="absolute left-0 top-0 p-14 px-16">
+  function handlePreviousClick() {
+    if (ctx.index <= 0) return;
+    ctx.setIndex((prev) => prev - 1);
+  }
+
+  return (
+    <button
+      aria-label="Previous"
+      type="button"
+      onMouseDown={() => ctx.setStatus("pressing-previous")}
+      onMouseUp={() => ctx.setStatus("idle")}
+      onClick={handlePreviousClick}
+      className="group absolute left-0 top-0 isolate flex h-full w-1/3 items-center pl-8"
+    >
+      <div className="size-full absolute left-0 top-0 opacity-0 backdrop-blur-md transition-opacity [mask:linear-gradient(90deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_100%)] group-hover:opacity-100" />
+      <div className="size-full absolute left-0 top-0 bg-gradient-to-r from-black/20 opacity-0 transition-opacity group-hover:opacity-100" />
+      <ChevronLeft className="size-8 z-10 text-white/30 transition-colors group-hover:text-white" />
+    </button>
+  );
+}
+
+function NextButton() {
+  const ctx = React.useContext(Context);
+
+  function handleNextClick() {
+    if (ctx.index >= 3) return;
+    ctx.setIndex((prev) => prev + 1);
+  }
+
+  return (
+    <button
+      aria-label="Next"
+      type="button"
+      onMouseDown={() => ctx.setStatus("pressing-next")}
+      onMouseUp={() => ctx.setStatus("idle")}
+      onClick={handleNextClick}
+      className="group absolute right-0 top-0 isolate flex h-full w-1/3 items-center justify-end pr-8"
+    >
+      <div className="size-full absolute left-0 top-0 opacity-0 backdrop-blur-md transition-opacity [mask:linear-gradient(270deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_100%)] group-hover:opacity-100" />
+      <div className="size-full absolute left-0 top-0 bg-gradient-to-l from-black/20 opacity-0 transition-opacity group-hover:opacity-100" />
+      <ChevronRight className="size-8 z-10 text-white/30 transition-colors group-hover:text-white" />
+    </button>
+  );
+}
+
+function InnerContent() {
+  const ctx = React.useContext(Context);
+  const location = locationData[ctx.index];
+  const isPressingNext = ctx.status === "pressing-next";
+  const isPressingPrevious = ctx.status === "pressing-previous";
+
+  return (
+    <div className="size-[600px] relative isolate overflow-hidden rounded-[100px] border border-[#cacaca] bg-[#e3e3e3] font-medium tracking-tight shadow-[0_3px_3px_-1.5px_rgba(0,0,0,0.06)]">
+      <AnimatePresence mode="popLayout">
+        <motion.div
+          key={ctx.index}
+          initial={{
+            // opacity: 0,
+            filter: "blur(8px)",
+            transform: "perspective(1000px) rotateY(0deg)",
+          }}
+          animate={
+            isPressingNext
+              ? {
+                  transform: "perspective(800px) rotateY(15deg)",
+                  // opacity: 1,
+                  filter: "blur(0px)",
+                }
+              : isPressingPrevious
+                ? {
+                    transform: "perspective(800px) rotateY(-15deg)",
+                    // opacity: 1,
+                    filter: "blur(0px)",
+                  }
+                : { filter: "blur(0px)" }
+          }
+          exit={{ filter: "blur(8px)" }}
+          className="size-full relative"
+        >
+          <Image
+            src={location.image}
+            alt={location.title}
+            className="size-full scale-150 object-cover"
+          />
+        </motion.div>
+      </AnimatePresence>
+
+      <div className="absolute left-0 top-0 z-0 h-1/2 w-full backdrop-blur-md [mask:linear-gradient(180deg,rgba(0,0,0,1)_0%,rgba(0,0,0,0)_100%)]" />
+      <div className="absolute left-0 top-0 z-0 h-1/2 w-full bg-gradient-to-b from-black/40" />
+
+      <motion.div
+        initial={{
+          transform: "perspective(1000px) rotateY(0deg)",
+        }}
+        animate={
+          isPressingNext
+            ? {
+                transform: "perspective(800px) rotateY(20deg)",
+              }
+            : isPressingPrevious
+              ? {
+                  transform: "perspective(800px) rotateY(-20deg)",
+                }
+              : {}
+        }
+        className="absolute left-0 top-0 z-20 w-full p-14 px-16"
+      >
         <p className="text-3xl font-normal leading-[1.4] tracking-wide text-white">
-          Snowy Barn
+          {location.title}
           <br />
-          Paradise
+          {location.city}
           <br />
-          WA
+          {location.state}
         </p>
-      </div>
-      <div className="absolute right-0 top-0 flex gap-1 p-[75px] px-16">
-        <div className="h-[3px] w-3.5 rounded-full bg-white/30" />
-        <div className="h-[3px] w-3.5 rounded-full bg-white/30" />
-        <div className="h-[3px] w-3.5 rounded-full bg-white" />
-        <div className="h-[3px] w-3.5 rounded-full bg-white/30" />
-      </div>
+      </motion.div>
+
+      <NavigationIndicator />
+
+      <PreviousButton />
+
+      <NextButton />
     </div>
   );
 }
 
 export default function HomePage() {
+  const [index, setIndex] = React.useState(2);
   const [status, setStatus] = React.useState("idle");
 
   React.useEffect(() => {
@@ -59,7 +235,7 @@ export default function HomePage() {
   }, [setStatus]);
 
   return (
-    <Context.Provider value={{ status, setStatus }}>
+    <Context.Provider value={{ index, setIndex, status, setStatus }}>
       <MotionConfig transition={transition}>
         <main className="relative flex h-screen items-center justify-center">
           <InnerContent />
