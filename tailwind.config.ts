54f50cd0fd3fac1b7a9894dcf858f9ac7b107ffb
vaunblu <vaunblu@proton.me>
2024-09-10 16:00:35 -0400

    feat: create new page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,6 +66,9 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      boxShadow: {
+        mixed: "var(--shadow-mixed)",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
